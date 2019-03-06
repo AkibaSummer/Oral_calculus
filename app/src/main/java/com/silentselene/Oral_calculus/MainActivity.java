37ac2928376b12bab903b153f6425f256df4f45d
AkibaSummer <oopsvpwowlq@gmail.com>
2019-03-06 23:31:42 +0800

    add activity

--- app/src/main/java/com/silentselene/Oral_calculus/MainActivity.java
+++ app/src/main/java/com/silentselene/Oral_calculus/MainActivity.java
@@ -1,14 +1,20 @@
 package com.silentselene.Oral_calculus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int problemnums = 13;
+    public static final int problemtypes = 1;
+    public static int score = 0;
+    public static int type = 0;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -39,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/silentselene/Oral_calculus/TestActivity.java
+++ app/src/main/java/com/silentselene/Oral_calculus/TestActivity.java
@@ -1,7 +1,40 @@
 package com.silentselene.Oral_calculus;
 
-import android.app.AppComponentFactory;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class TestActivity  extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
+
+    //    TextView scoreT = findViewById(R.id.score);
+//    TextView nowNumT = findViewById(R.id.nownum);
+//    TextView problemT = findViewById(R.id.problem);
+//    TextView timesT = findViewById(R.id.times);
+    int nowNum = 1, score = 0;
+//    Ret nowProblem;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        MainActivity.score = -1;
+
+//        EditText answerE = (EditText) findViewById(R.id.answer);
+//        answerE.requestFocus();
+//        nowProblem = getProblem.get(MainActivity.problemtypes);
+        updateView();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void updateView() {
+        TextView scoreT = findViewById(R.id.score);
+        TextView nowNumT = findViewById(R.id.nownum);
+        TextView problemT = findViewById(R.id.problem);
+        nowNumT.setText("第" + nowNum + "/" + MainActivity.problemnums + "题");
+//        problemT.setText(nowProblem.problem);
+        scoreT.setText(String.valueOf(score));
+    }
 }
