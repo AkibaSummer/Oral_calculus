c4694361773c4385913be7e5088219ab9e6e145e
AkibaSummer <oopsvpwowlq@gmail.com>
2019-03-09 23:38:43 +0800

    complete home action

--- app/src/main/java/com/silentselene/Oral_calculus/DashboardFragment.java
+++ app/src/main/java/com/silentselene/Oral_calculus/DashboardFragment.java
@@ -7,6 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class DashboardFragment extends Fragment {
+
+    int[] id = {R.id.button2_1,
+            R.id.button2_2,
+            R.id.button2_3,
+            R.id.button2_4,
+            R.id.button2_5,
+            R.id.button2_6,
+            R.id.button2_7,
+            R.id.button2_8,
+            R.id.button2_9,
+            R.id.button2_10,
+            R.id.button2_11,
+            R.id.button2_12,
+            R.id.button2_13};
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_dashbord,container,false);
